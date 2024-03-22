ae890cd5c0765d4dafa8a26b718f2dac69c2a3b7
Maria Julia Cavalcanti <majucavalcantix@gmail.com>
2024-03-22 20:30:31 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//@niallivmi
+using System;
+
+namespace Pgc1Aula02Ex01
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2;
+
+            Console.Write("Digite a nota do primeiro bimestre: ");
+            n1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota do segundo bimestre: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("A nota do estudante é: " + ((n1 + n2) / 2));
+        }
+    }
+}
